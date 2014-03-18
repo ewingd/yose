35987399c50e3255c517ae684de956272d1d154e
David Ewing <davidewing@neo.rr.com>
2014-03-18 17:40:57 -0400

    Added solution for powers of 2 challenge

--- hello.py
+++ hello.py
@@ -2,8 +2,10 @@ import os
 from flask import Flask
 from flask import Response
 from flask import render_template
+from flask import request
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route('/')
 def hello():
@@ -16,3 +18,14 @@ def ping():
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+@app.route('/primefactors')
+def prime_factors():
+    number = int(request.args.get('number'))
+    factors = get_prime_factors(number)
+    json = '{"number" : %s, "decomposition" : %s}' % (str(number), factors)
+    return Response(json, mimetype='application/json')
+
+
+def get_prime_factors(number):
+    return [2] * (number / 2)

6cad4ceade6237ac65f0b79fc1b7cf7f57c43bfd
David Ewing <davidewing@neo.rr.com>
2014-03-21 08:34:27 -0400

    Added support for roman numerals in input and output

--- hello.py
+++ hello.py
@@ -4,10 +4,71 @@ from flask import Response
 from flask import render_template
 from flask import request
 
+from collections import OrderedDict
+
 import json
 
 app = Flask(__name__)
 app.debug = True
+
+def to_roman(numbers):
+    output = []
+    values = OrderedDict([('C', 100),
+                          ('XC', 90),
+                          ('L', 50),
+                          ('XL', 40),
+                          ('X', 10),
+                          ('IX', 9),
+                          ('V', 5),
+                          ('IV', 4),
+                          ('I', 1)])
+
+
+    for number in numbers:
+        s = ''
+        for k, v in values.iteritems():
+            while number >= v:
+                number -= v
+                s = s + k
+        output.append(s)
+
+    return output
+
+def from_roman(number):
+    t = type(number).__name__
+    if t != 'str' and t != 'unicode':
+        raise ValueError('Not a valid roman numeral')
+
+    output = 0
+    values = OrderedDict([('C', 100),
+                          ('XC', 90),
+                          ('L', 50),
+                          ('XL', 40),
+                          ('X', 10),
+                          ('IX', 9),
+                          ('V', 5),
+                          ('IV', 4),
+                          ('I', 1)])
+    while number:
+        orig_number = number
+        for k, v in values.iteritems():
+            if number.startswith(k):
+                output += v
+                number = number[len(k):]
+        if orig_number == number:
+            raise ValueError('Not a valid roman numeral')
+    return output
+
+def romanize(function):
+    def wrapper(number):
+        try:
+            dec_number = from_roman(number)
+            result = function(dec_number)
+            result = to_roman(result)
+        except ValueError:
+            result = function(number)
+        return result
+    return wrapper
 
 @app.route('/')
 def hello():
@@ -47,20 +108,25 @@ def process_prime_factors(numbers):
     result = []
     for number in numbers:
         try:
-            number = int(number)
-            if number > 1000000:
-                output = {"number" : number, "error" : "too big number (>1e6)"}
-            elif number < 2:
-                output = {"number" : number, "error" : "not an integer > 1"} 
-            else:
-                factors = get_prime_factors(number)
-                output = {"number" : number, "decomposition" : factors}
-        except ValueError:
-            output = {"number" : number, "error" : "not a number"}
+            factors = get_prime_factors(number)
+            output = {"number" : number, "decomposition" : factors}
+        except ValueError, e:
+            output = {"number" : number, "error" : "%s" % (e.message)}
         result.append(output)
     return result
 
+@romanize
 def get_prime_factors(number):
+    try:
+        number = int(number)
+    except ValueError:
+        raise ValueError("not a number")
+
+    if number > 1000000:
+        raise ValueError("too big number (>1e6)")
+    elif number < 2:
+        raise ValueError("not an integer > 1")
+
     factors = []
     current_number = 2
 
